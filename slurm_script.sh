25baf01adc0ac4b8123a7a10cac17f0c11f6ee76
ufkpt@student.kit.edu <ufkpt@uc2n997.localdomain>
2022-12-24 15:06:54 +0100

    first commit

---
+++ slurm_script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#echo $PATH
+PATH=$PATH:"/opt/bwhpc/common/devel/miniconda3/bin:/opt/bwhpc/common/devel/miniconda3/condabin"
+#echo $PATH
+source activate cGAN 
+#DEBUG_PATH='/home/kit/stud/ufkpt/NN_pr/cGAN_env.debug'
+export LD_LIBRARY_PATH="/home/kit/stud/ufkpt/.conda/envs/cGAN/lib:"$LD_LIBRARY_PATH
+# conda list > $DEBUG_PATH
+# pip list >> $DEBUG_PATH
+# which pip >> $DEBUG_PATH
+# which python3 >> $DEBUG_PATH
+python3 /home/kit/stud/ufkpt/NN_pr/train.py &> /home/kit/stud/ufkpt/NN_pr/trainig.log
