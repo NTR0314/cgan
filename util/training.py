8189c831cc37729b711a6539b92cb0b0414fe1ef
Oswald Zink <oswaldzink1@gmail.com>
2023-01-01 17:53:48 +0100

    v2 debug

--- util/training.py
+++ util/training.py
@@ -96,6 +96,7 @@ def train_model(model_path, num_epochs, batch_size, workers, netD, netG, nz, lr,
             # Classify all fake batch with D
             output = netD(fake.detach(), real_img_labels).view(-1)
             # Calculate D's loss on the all-fake batch
+            print(output, label)
             errD_fake = criterion(output, label)
             # Calculate the gradients for this batch, accumulated (summed) with previous gradients
             errD_fake.backward()
